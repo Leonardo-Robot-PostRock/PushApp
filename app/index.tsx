fa8508613119c4256cdf54772707db3b8e8a548f
Leonardo <leonardo38361@gmail.com>
2025-12-07 10:34:32 -0300

    Configuraciones y testeo de notificaciones únicamente en android

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import { ThemedText } from '@/components/themed-text'
-import usePushNotifications from '@/hooks/usePushNotifications'
-import React from 'react'
-import { View } from 'react-native'
+import { ThemedText } from '@/components/themed-text';
+import usePushNotifications from '@/hooks/usePushNotifications';
+import React from 'react';
+import { FlatList, View } from 'react-native';
 
 const PushApp = () => {
 
-    const {} = usePushNotifications();
+    const { expoPushToken, notifications } = usePushNotifications();
 
     return (
         <View style={{ marginHorizontal: 10, marginTop: 5 }}>
-            <ThemedText>Token: </ThemedText>
+            <ThemedText style={{
+                marginTop: 10,
+                fontWeight: 'bold'
+            }}>
+                Notificaciones
+            </ThemedText>
+
+            <FlatList
+                data={notifications}
+                keyExtractor={(item) => item.request.identifier}
+                renderItem={({ item }) => (
+                    <View>
+                        <ThemedText style={{ fontWeight: 'bold' }}>{item.request.content.title}</ThemedText>
+                        <ThemedText style={{ fontWeight: 'bold' }}>{item.request.content.body}</ThemedText>
+                        <ThemedText style={{ fontWeight: 'bold' }}>{JSON.stringify(item.request.content.data, null, 2)}</ThemedText>
+                    </View>
+                )}
+                ItemSeparatorComponent={() => (
+                    <View
+                        style={{ height: 1, backgroundColor: 'grey', opacity: 0.3 }}
+                    />
+                )}
+            />
         </View>
     )
 }
